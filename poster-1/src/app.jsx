46337ec3af0d1432702d12f22c2e357076524f31
Ashique <ashique9496@gmail.com>
2024-03-30 18:14:43 +0530

    Poster Completed

---
+++ poster-1/src/app.jsx
@@ -0,0 +1,18 @@
+import "./app.scss";
+
+export function App() {
+    return (
+        <>
+            <div class="container">
+                <div class="number">
+                    <div class="one-stroke"></div>
+                    <div class="one"></div>
+                    <div class="zero zero1"></div>
+                    <div class="zero zero2"></div>
+                </div>
+                <p class="text-big">days</p>
+                <p class="text-small">css challanges</p>
+            </div>
+        </>
+    );
+}
